4860a0dd9f2c3343c4f245fd1ea475a2382dfcab
SaurabhVerma200 <102269656+SaurabhVerma200@users.noreply.github.com>
2022-10-27 20:13:16 -0700

    Add files via upload

---
+++ ProjectM4A1/schemas/loanSchema.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const loanSchema = new Schema({
+  customerName:  String, 
+  phoneNumber: Number,
+  address:   String,
+  loanAmount: Number,
+  interest: Number,
+  loanTermYears: Number,
+  loanType: String,
+  description: String,
+  createdDate: { type: Date, default: Date.now },
+  insertedDate: { type: Date, default: Date.now },
+});
+
+const loans = mongoose.model('Loan', loanSchema);
